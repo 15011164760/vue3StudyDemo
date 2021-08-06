d11a6a0df27461a46ecbafe0153351e3f1ee9e3e
yangdongxu <1152718127@qq.com>
2021-08-06 17:26:03 +0800

    ref 获取元素

---
+++ src/1-5解决: 利用 toRefs .vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <!-- <div>{{ obj }}</div> -->
+    <div>
+      {{ name }}
+    </div>
+    <div>
+      {{ age }}
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+//1-5解决: 利用 toRefs 可以将一个响应式 reactive 对象的所有原始属性转换为响应式的 ref 属性
+export default defineComponent({
+  setup() {
+    let obj = reactive({
+      name: "yandonx",
+      age: 13,
+      cars: ["huoc", "good", "hell"],
+    });
+    setInterval(() => {
+      obj.name += "=";
+      obj.age += 1;
+    }, 2000);
+    // let {name,age}=toRefs(obj);
+    // let allData=toRefs(obj);
+    let allData=toRefs(obj);
+    return {
+      ...allData,
+      obj,
+      // name,age
+    };
+  },
+});
+</script>
+
+
+<style>
+</style>
---
+++ src/1-6ref==获取元素.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <input type="text" ref="inputRef" />
+    <input type="text" ref="inputRef1" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, onMounted } from "vue";
+// ref 获取元素
+export default defineComponent({
+  setup() {
+    const inputRef = ref<HTMLElement | null>(null);
+    const inputRef1 = ref<HTMLElement | null>(null);
+    onMounted(() => {
+      inputRef.value && inputRef.value.focus();
+      console.log(inputRef.value);
+    });
+    return {
+      inputRef,
+      inputRef1,
+    };
+  },
+});
+</script>
+
+
+<style>
+</style>
