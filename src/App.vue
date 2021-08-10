a494bb32806578fdbe24af2cbd4cd378a25ded57
yangdongxu <1152718127@qq.com>
2021-08-10 22:41:41 +0800

    Teleport 提供了一种干净的方法, 让组件的 html 在父组件界面外的特定标签(很可能是 body)下插入显示

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,21 @@
 <template>
-  <Son/>
+  <modelBtn/>
   <button @click="changeFn">changeFn</button>
 </template>
 
 <script lang="ts">
 import { defineComponent, isProxy, isReactive, isReadonly, isRef, provide, reactive, readonly, ref } from 'vue'
-import Son from './components/Son.vue'
+import modelBtn from './components/modelBtn.vue'
 export default defineComponent({
   components:{
-    Son
+    modelBtn
   },
   setup() {
-    //六个月 
-    /* isRef: 检查一个值是否为一个 ref 对象
-isReactive: 检查一个对象是否是由 reactive 创建的响应式代理
-isReadonly: 检查一个对象是否是由 readonly 创建的只读代理
-isProxy: 检查一个对象是否是由 reactive 或者 readonly 方法创建的代理 */
+    /* 
+    isRef: 检查一个值是否为一个 ref 对象
+    isReactive: 检查一个对象是否是由 reactive 创建的响应式代理
+    isReadonly: 检查一个对象是否是由 readonly 创建的只读代理
+    isProxy: 检查一个对象是否是由 reactive 或者 readonly 方法创建的代理 */
     const color=ref('red');
     const color1=reactive({
       color:'red'
---
+++ src/components/modelBtn.vue
@@ -0,0 +1,28 @@
+<template>
+<button @click="openBool=true">openBool</button>
+<!-- 瞬移组建  to移动到哪个标签中body是到body中  #model是移动到id是model的元素中 -->
+  <teleport to="body">
+     <div v-if="openBool" class="modelS">
+         <div>modelStyle</div>
+         <button @click="openBool=false">closeFn</button>
+     </div>
+  </teleport>
+</template>
+
+<script>
+import { defineComponent,ref } from 'vue'
+
+export default defineComponent({
+    setup() {
+        let openBool=ref(false)
+        return{
+            openBool
+        }
+    },
+})
+</script>
+
+
+<style>
+
+</style>
